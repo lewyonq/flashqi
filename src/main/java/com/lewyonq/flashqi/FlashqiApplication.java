008d10ef4b9e5bddbcc5f8116809af657a0b8024
lewyonq <jakub.lewandowski98@gmail.com>
2025-01-08 19:37:29 +0100

    init flashqi repo

---
+++ src/main/java/com/lewyonq/flashqi/FlashqiApplication.java
@@ -0,0 +1,13 @@
+package com.lewyonq.flashqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lewyonq/flashqi/FlashqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewyonq.flashqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
